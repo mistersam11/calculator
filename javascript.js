bc96b9db498a6e8c2514ee98c5f09bb5a5c102b1
Sam <samroth011@gmail.com>
2025-09-16 21:52:12 -0400

    Define operate function to calculate current nums

--- javascript.js
+++ javascript.js
@@ -13,3 +13,27 @@ function multiplyNumbers(a,b) {
 function divideNumbers(a,b) {
     return a / b;
 }
+
+let operator = '';
+let num1;
+let num2;
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return addNumbers(num1, num2);
+            break;
+        case '-':
+            return subtractNumbers(num1, num2);
+            break;
+        case '*':
+            return multiplyNumbers(num1, num2);
+            break;
+        case '/':
+            return divideNumbers(num1, num2);
+            break;
+        default:
+            return 'Error: operator is invalid.'
+    }
+}
+
