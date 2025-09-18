484795b98a54b4623f40691d72ecc830525a66c1
Sam <samroth011@gmail.com>
2025-09-17 22:29:33 -0400

    Create event listener and logic for numbers

--- javascript.js
+++ javascript.js
@@ -37,3 +37,83 @@ function operate(operator, num1, num2) {
     }
 }
 
+const displayFigure = document.querySelector('#displayFigure');
+const numPad = document.querySelector('#numbers');
+const operatorContainer = document.querySelector('#operators');
+
+const ONE = document.querySelector('#one');
+const TWO = document.querySelector('#two');
+const THREE = document.querySelector('#three');
+const FOUR = document.querySelector('#four');
+const FIVE = document.querySelector('#five');
+const SIX = document.querySelector('#six');
+const SEVEN = document.querySelector('#seven');
+const EIGHT = document.querySelector('#eight');
+const NINE = document.querySelector('#nine');
+const ZERO = document.querySelector('#zero');
+
+const CLEAR = document.querySelector('#clear');
+const ADD = document.querySelector('#addition');
+const SUBTRACT = document.querySelector('#subtract');
+const MULTIPLY = document.querySelector('#multiply');
+const DIVIDE = document.querySelector('#divide');
+const EQUALS = document.querySelector('#equals');
+
+let currentSelection = [];
+function reduceSelection(array) {
+    return array.join('');
+
+}
+
+numPad.addEventListener('mousedown', (e) => {
+    const target = e.target;
+    if (currentSelection.length <= 10) {
+        switch (target.id) {
+            case 'one':
+                currentSelection.push(1);
+                displayFigure.textContent = reduceSelection(currentSelection);
+                break;
+            case 'two':
+                currentSelection.push(2);
+                displayFigure.textContent = reduceSelection(currentSelection);
+                break;
+            case 'three':
+                currentSelection.push(3);
+                displayFigure.textContent = reduceSelection(currentSelection);
+                break;
+            case 'four':
+                currentSelection.push(4);
+                displayFigure.textContent = reduceSelection(currentSelection);
+                break;
+            case 'five':
+                currentSelection.push(5);
+                displayFigure.textContent = reduceSelection(currentSelection);
+                break;
+            case 'six':
+                currentSelection.push(6);
+                displayFigure.textContent = reduceSelection(currentSelection);
+                break;
+            case 'seven':
+                currentSelection.push(7);
+                displayFigure.textContent = reduceSelection(currentSelection);
+                break;
+            case 'eight':
+                currentSelection.push(8);
+                displayFigure.textContent = reduceSelection(currentSelection);
+                break;
+            case 'nine':
+                currentSelection.push(9);
+                displayFigure.textContent = reduceSelection(currentSelection);
+                break;
+            case 'zero':
+                currentSelection.push(0);
+                displayFigure.textContent = reduceSelection(currentSelection);
+                break;
+            case 'numbers':
+                break;
+            default:
+                displayFigure.textContent = 'ERR';
+                break;
+        }
+    }
+});
